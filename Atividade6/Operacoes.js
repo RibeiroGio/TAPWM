bc471c101e30e332a0be03d5e909ceef8f0c9476
Gio_Ribeiro <95325532+RibeiroGio@users.noreply.github.com>
2023-09-06 17:44:23 -0300

    Add files via upload

---
+++ Atividade6/Operacoes.js
@@ -0,0 +1,12 @@
+
+var num1 = parseFloat(prompt("Informe o primeiro número: "));
+var num2 = parseFloat(prompt("Informe o segundo número: "));
+var soma = num1 + num2;
+var produto = num1 * num2;
+var divisao = num1 / num2;
+var resto = num1%num2;
+
+
+alert("a soma dos numeros é: " + soma + " ,o produto é: " + produto + 
+" ,a divisao é: " + divisao + " ,e o resto da divisão é: " +resto);
+
---
+++ Atividade6/media.js
@@ -0,0 +1,9 @@
+
+var nome;
+
+var n1 = parseFloat(prompt("Informe a primeira nota do aluno: "));
+var n2 = parseFloat(prompt("Informe a segunda nota do aluno: "));
+var n3 = parseFloat(prompt("Informe a terceira nota do aluno: "));
+
+var media = ((n1)+(n2)+(n3))/3;
+alert("A média das notas é: " + media);
